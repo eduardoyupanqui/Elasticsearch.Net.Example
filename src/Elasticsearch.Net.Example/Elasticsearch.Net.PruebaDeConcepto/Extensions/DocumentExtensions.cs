f57299e5fd51051d956a44cc92685d4663642757
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2020-04-05 15:21:15 -0500

    Ejemplo: RegistrarDocumentModel

---
+++ src/Elasticsearch.Net.Example/Elasticsearch.Net.PruebaDeConcepto/Extensions/DocumentExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Elasticsearch.Net.PruebaDeConcepto.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Elasticsearch.Net.PruebaDeConcepto.Extensions
+{
+    public static class DocumentExtensions
+    {
+        /// <summary>
+        /// Procedimiento de "obtención de cambios" desde un bloque de metadatos origen a uno destino.
+        /// Solo se consideran como cambios los campos con valores distintos a null
+        /// La lista de administrados se reemplaza
+        /// </summary>
+        /// <param name="destination"></param>
+        /// <param name="changes"></param>
+        public static void MergeFrom(this DocumentModel destination, DocumentModel changes)
+        {
+            if (changes == null) return;
+            //id_bloque no se combina
+            //id_flujo no se combina
+
+            //Mezcla ids_procesos:
+
+            //Asegurando que las listas tengan valor
+            destination.ids_procesos = destination.ids_procesos ?? new List<string>();
+            changes.ids_procesos = changes.ids_procesos ?? new List<string>();
+            //Mezclando ids_procesos
+            destination.ids_procesos = destination.ids_procesos.Union(changes.ids_procesos).ToList();
+
+            //Remplazo de administrados
+            destination.administrados = (changes.administrados != null && changes.administrados.Any()) ? changes.administrados : destination.administrados;
+
+            destination.solici_numero = changes.solici_numero ?? destination.solici_numero;
+            destination.solici_id_estado = changes.solici_id_estado ?? destination.solici_id_estado;
+            destination.solici_fecha_registro = changes.solici_fecha_registro ?? destination.solici_fecha_registro;
+
+        }
+    }
+}
--- src/Elasticsearch.Net.Example/Elasticsearch.Net.PruebaDeConcepto/Program.cs
+++ src/Elasticsearch.Net.Example/Elasticsearch.Net.PruebaDeConcepto/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Elasticsearch.Net.PruebaDeConcepto.Config;
 using Elasticsearch.Net.PruebaDeConcepto.Entidades;
+using Elasticsearch.Net.PruebaDeConcepto.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Nest;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,15 +15,21 @@ namespace Elasticsearch.Net.PruebaDeConcepto
     {
         static void Main(string[] args)
         {
-
+            //Install https://www.elastic.co/es/downloads/past-releases/elasticsearch-6-5-4
+            //>docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.5.4
+            //O
+            //>docker-compose up
             IConfiguration config = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", true, true)
                 .Build();
             var _elasticConfig = config.GetSection(nameof(ElasticConfig)).Get<ElasticConfig>();
 
             IServiceCollection services = new ServiceCollection();
-            services.AddSingleton<ElasticClient>((sp) => {
-                var settings = new ConnectionSettings(new Uri(_elasticConfig.ELASTIC_SERVER_URL)).DefaultIndex(_elasticConfig.METADATA_INDEX);
+            services.AddSingleton<IElasticClient>((sp) => {
+                var settings = new ConnectionSettings(new Uri(_elasticConfig.ELASTIC_SERVER_URL))
+                    //.DefaultIndex("defaultindex")
+                    .DefaultMappingFor<DocumentModel>(m => m.IndexName("metadata_index"));
+
                 //settings.BasicAuthentication(_elasticConfig.ELASTIC_USER, _elasticConfig.ELASTIC_PASS);
 #if DEBUG
                 settings.DisableDirectStreaming(); //deshabilitado el streaming para capturar los request y response y examinarlos
@@ -30,15 +38,66 @@ namespace Elasticsearch.Net.PruebaDeConcepto
             });
 
             Console.WriteLine("Hello World Elasticsearch.Net!");
+            //Obteniendo el Client
+            var serviceProvider = services.BuildServiceProvider();
+            IElasticClient _elasticClient = serviceProvider.GetService<IElasticClient>();
 
+            //1 Registrar documento en el indice
+            foreach (var request in DummyData.ObtenerSolicitudesDummy1())
+            {
+                RegistrarDocumentModel(_elasticClient, request);
+            }
 
-
-
+            //2 Buscar documento en el indice
         }
 
-        public void RegistrarBloqueMetadatos(DocumentModel request) 
-        {
 
+
+        public static void RegistrarDocumentModel(IElasticClient _elasticClient, DocumentModel request) 
+        {
+            
+            //1) obtener documento que tenga el mismo "id_proceso_base" 
+            var filters = new List<Func<QueryContainerDescriptor<DocumentModel>, QueryContainer>>();
+            filters.Add(fq => fq.Match(w => w.Field("id_proceso_base").Query(request.id_proceso_base)));
+
+            ISearchResponse<DocumentModel> searchResponse = _elasticClient.Search<DocumentModel>(x =>
+                x.Query(q => q.Bool(bq => bq.Filter(filters))));
+
+            //2) si ya existe, se obtiene
+            
+
+            if (searchResponse.Hits.Count == 0)
+            {
+                //Crear
+                var insertResponse = _elasticClient.Index<DocumentModel>(new IndexRequest<DocumentModel>(request));
+                if (!insertResponse.IsValid)
+                {
+                    Console.WriteLine("Error al registrar bloque");
+                    Console.WriteLine(insertResponse.DebugInformation);
+                }
+
+            }
+            else
+            {
+                //Modificar
+                IHit<DocumentModel> hit = searchResponse.Hits.First();
+                string bloque_elasticId = hit.Id;
+                DocumentModel bloque = hit.Source;
+                bloque.MergeFrom(request);
+
+                var updateResponse = _elasticClient.Update<DocumentModel>(DocumentPath<DocumentModel>.Id(bloque_elasticId),
+                    u => u
+                        //.Index("metadata_index")
+                        //.Type("_doc")
+                        .DocAsUpsert(true)
+                        .Doc(bloque)
+                    );
+                if (!updateResponse.IsValid)
+                {
+                    Console.WriteLine("Error al actualizar bloque");
+                    Console.WriteLine(updateResponse.DebugInformation);
+                }
+            }
         }
 
         public IEnumerable<BuscarMetadatosResponse> BuscarMetadatos(BuscarMetadatosRequest request) 
