6a70948271fd61576fa7db4306da427c9626258b
Eduardo Manuel Yupanqui Quispe <eduardo.yupanqui@outlook.com>
2020-04-04 23:43:47 -0500

    Primer commit

---
+++ src/Elasticsearch.Net.Example/Elasticsearch.Net.PruebaDeConcepto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Elasticsearch.Net.PruebaDeConcepto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Elasticsearch.Net!");
+        }
+    }
+}
